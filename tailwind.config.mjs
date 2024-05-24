b72b102b990e98f5e3750c28b6d7ab62400182b6
mouro <dolbrebr@gmail.com>
2024-05-24 08:28:02 -0300

    chore: criado setup do projeto e o componente inicial chamado hero

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+import presetQuick from "franken-ui/shadcn-ui/preset-quick";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	presets: [presetQuick({  })],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+
+		
+	],
+}
